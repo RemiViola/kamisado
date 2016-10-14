7f0da8c1d19c62fee4f167504a96f6009b5222ff
Remi Viola <remi@lenovo.remi>
2016-10-14 13:26:04 +0200

    k2 avec liste simple pour le plateau

--- ia.pl
+++ ia.pl
@@ -18,7 +18,7 @@ test_couleur_bloquee(b,Coul,L,L_):-
     not(memberchk([1,_,_],L_coup_possible)),
     append([],L,L_).
 
-/*Liste des cases où le joueur ne peut pas s'arreter*/
+/*Liste des cases où le joueur ne peut pas s'arreter                  /!\problème si le déplacement change cette liste*/
 liste_couleur_bloquee(J,L):-
     test_couleur_bloquee(J,br,[],L1),
     test_couleur_bloquee(J,gr,L1,L2),
@@ -29,7 +29,7 @@ liste_couleur_bloquee(J,L):-
     test_couleur_bloquee(J,bl,L6,L7),
     test_couleur_bloquee(J,or,L7,L).
 
-/*Liste des cases où le joueur peut s'arreter*/
+/*Liste des cases où le joueur peut s'arreter                         idem*/
 liste_couleur_autorisee(J,L):-
     liste_couleur_bloquee(J,L_),
     subtract([br,gr,re,ye,pi,pu,bl,or],L_,L).
